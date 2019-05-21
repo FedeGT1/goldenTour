f2ba15baa706d68d69c984e2bce2dd452ec3eff6
dario <dario@DESKTOP-51LG57U.station>
2019-05-21 15:13:16 +0200

    Sistemazione genericDao e implementazione

--- goldenTour/src/com/goldentour/jee/dao/GenericDao.java
+++ goldenTour/src/com/goldentour/jee/dao/GenericDao.java
@@ -1,8 +1,23 @@
 package com.goldentour.jee.dao;
 
 import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.EntityManager;
 
 public interface GenericDao<T>  {
+	
+	T create(T t);
+	
+	void delete(Object id);
+	
+	T find(Object id);
+	
+	T update(T t);
+	
+	List<T> findAll();
+	
+	EntityManager getEntityManager();
 	
 	public Object findById(Serializable id);
 
--- goldenTour/src/com/goldentour/jee/dao/impl/GenericDaoImpl.java
+++ goldenTour/src/com/goldentour/jee/dao/impl/GenericDaoImpl.java
@@ -3,10 +3,13 @@ package com.goldentour.jee.dao.impl;
 import java.io.Serializable;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
+import java.util.List;
 
-import javax.annotation.PostConstruct;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 
 import org.springframework.stereotype.Repository;
 
@@ -25,7 +28,7 @@ public class GenericDaoImpl<T> implements GenericDao<T> {
 		
 		Type t = getClass().getGenericSuperclass();
 		ParameterizedType pt = (ParameterizedType) t;
-		type = (Class) pt.getActualTypeArguments()[0];
+		type = (Class<T>) pt.getActualTypeArguments()[0];
 		
 	}
 
@@ -33,6 +36,46 @@ public class GenericDaoImpl<T> implements GenericDao<T> {
 	public Object findById(Serializable id) {
 		Object obj = em.find(type, id);
 		return obj;
+	}
+
+	@Override
+	public T create(final T t) {
+this.em.persist(t);
+return t;
+	}
+
+	@Override
+	public void delete(final Object id) {
+ this.em.remove(this.em.getReference(type, id));		
+	}
+
+	@Override
+	public T find(final Object id) {
+		return this.em.find(type,  id);
+		
+	}
+
+	@Override
+	public T update(final T t) {
+		return this.em.merge(t);
+	}
+
+	@Override
+	public List<T> findAll() {
+				CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
+
+				CriteriaQuery<T> criteriaQuery = criteriaBuilder.createQuery(type);
+
+				Root<T> root = criteriaQuery.from(type);
+
+				criteriaQuery.select(root);
+
+				return em.createQuery(criteriaQuery).getResultList();
+	}
+
+	@Override
+	public EntityManager getEntityManager() {
+		return em;
 	}
 
 }
